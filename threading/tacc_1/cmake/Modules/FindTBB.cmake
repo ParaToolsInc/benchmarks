6dbf5caf52e155ed54c585e10a82bbd735c136f5
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2016-02-18 22:48:02 -0700

    better to have TBB variables

---
+++ threading/tacc_1/cmake/Modules/FindTBB.cmake
@@ -0,0 +1,40 @@
+# - Try to find TBB
+# Once done this will define
+#  TBB_FOUND - System has TBB
+#  TBB_INCLUDE_DIRS - The TBB include directories
+#  TBB_LIBRARY_DIRS - The pion library directories
+#  TBB_LIBRARIES - The libraries needed to use TBB
+
+# find_package (PkgConfig)
+# pkg_check_modules (PC_LIBTBB QUIET libpion)
+set (TBB_DEFINITIONS ${PC_LIBTBB_CFLAGS_OTHER})
+#set (EXT_LIBLOC_TBB ${CMAKE_SOURCE_DIR}/ext_library/linux/x86_64/pion-net-4.0.5)
+
+find_path (TBB_INCLUDE_DIRS
+  NAMES tbb/tbb_config.h
+  HINTS ${TBBROOT}/include ${INCLUDE}
+  )
+
+
+find_library (TBB_LIBRARY
+  NAMES tbb tbbmalloc tbbmalloc_proxy
+  HINTS ${TBBROOT}/lib ${LIBRARY_PATH}
+)
+
+set (TBB_LIBRARIES ${TBB_LIBRARY})
+foreach(_tbb_my_lib ${TBB_LIBRARY})
+  get_filename_component(_tbb_my_lib_path "${_tbb_my_lib}" PATH)
+  list(APPEND TBB_LIBRARY_DIRS ${_tbb_my_lib_path})
+endforeach()
+
+include (FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set TBB_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args (TBB DEFAULT_MSG
+  TBB_LIBRARIES
+  TBB_INCLUDE_DIRS
+  TBB_LIBRARY_DIRS
+  TBB_LIBRARY)
+
+mark_as_advanced (TBB_LIBRARY)
+
