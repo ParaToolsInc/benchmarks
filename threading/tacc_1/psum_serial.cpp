ab1f9a746646ddcc9bf56cb3d396229014d651e9
Srinath Vadlamani <srinathv@ucar.edu>
2016-02-19 16:45:35 -0600

    removed tsc() contstruct

--- threading/tacc_1/psum_serial.cpp
+++ threading/tacc_1/psum_serial.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <chrono>
-#include <inttypes.h>
 
 
 #define NFRAMES 500
@@ -11,13 +10,6 @@
 // to compare timing.  The reported results should be divided by the CPU frequency
 // to get wall time.
 
-typedef uint64_t tic;
-
-__inline__ tic tsc() {
-  tic lo, hi;
-  __asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
-  return ((tic)hi << 32 | lo);
-}
 
 int main(int a, char **b) {
   using namespace std::chrono;
@@ -30,7 +22,6 @@ int main(int a, char **b) {
   }
 
   {
-   // tic t0 = tsc();
    high_resolution_clock::time_point t1 = high_resolution_clock::now();
     for (int i = 0; i < NFRAMES; i++)
       for (int j = 1; j < NUMBUF; j++) {
@@ -39,10 +30,8 @@ int main(int a, char **b) {
         for (int k = 0; k < BUFSIZE; k++) *a++ += *b++;
       }
 
- //   tic ttics = tsc() - t0;
    high_resolution_clock::time_point t2 = high_resolution_clock::now();
    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-    //std::cout << "non-parallel " << ttics / (2.8 * 1000000000.0) << "\n";
     std::cout << "non-parallel " << time_span.count() << " seconds.\n";
   }
 
