dd9473c43b8b00f5080cc79e9786e8834600805f
Srinath Vadlamani <srinath.vadlamani@gmail.com>
2016-02-22 12:53:53 -0700

    argparse for help

--- threading/tacc_1/runTest_1.py
+++ threading/tacc_1/runTest_1.py
@@ -2,9 +2,11 @@
 
 #####
 ## loop over executables, loop over number of threads
-## stdout to file name <test>_N.txt
+## stdout to file name <machine>_<test>_N.txt
+## run in idev
 import subprocess
-import os,sys,getopt
+import sys,os,getopt,argparse,math
+
 
 def shellCommand(command,errorMessage):
 #command initiated where this script is ran
@@ -18,4 +20,26 @@ def shellCommand(command,errorMessage):
 
 testList=['serial','openmp','openmp_2','tbb_rankBuffer','tbb_singleBuffer']
 threadCount=[1,2,4,8,10,12,14,16,18,19,20,25,30,35,40]
+
+
+p=subprocess.check_output('hostname')
+machine=p.split('.')[1]
+
+
+def main():
+  parser = argparse.ArgumentParser(description='Run all the psum tests on Stampede or Maverick. '\
+                                                'calculate averages and standard deviations.'\
+                                                ' Also, a histogram will be generated for each type and thread count.'\
+                                                '  We also will produce a line plot for the avg. per type vs. thread count')
+  args = parser.parse_args()
+
+  currentDir=os.getcwd()
+
+  for test in testList:
+    print test
+
+
+if __name__ == "__main__":
+   main()
+
 
