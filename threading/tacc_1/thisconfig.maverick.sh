049e71856f7643d69830e9efeb8df025acd3d765
Srinath Vadlamani <srinathv@paratools.com>
2016-02-25 12:22:42 -0600

    config file need to included

---
+++ threading/tacc_1/thisconfig.maverick.sh
@@ -0,0 +1,7 @@
+rm -rf CMakeCache.txt CmakeFiles
+
+cmake \
+  -DCMAKE_CXX_COMPILER:FILEPATH=icpc \
+  -DCMAKE_C_COMPILER:FILEPATH=icc \
+  -DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -g -std=c++11 " \
+/work/02463/srinathv/maverick/benchmarks-paratools/threading/tacc_1
