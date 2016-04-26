8e5feef5ac7cd100c3f2c5ff74a0dcd698815289
Srinath Vadlamani <srinathv@paratools.com>
2016-04-25 21:15:07 -0700

    better flags for compile

--- poisson/sharedLibEx/thisconfig.sh
+++ poisson/sharedLibEx/thisconfig.sh
@@ -3,4 +3,4 @@ rm -rf CMakeFiles CMakeCache.txt
 cmake . \
 -DCMAKE_C_COMPILER=icc \
 -DCMAKE_CXX_COMPILER=icpc \
--DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -openmp"
+-DCMAKE_CXX_FLAGS="${CMAKE_CXX_FLAGS} -qopenmp -g -O3"
